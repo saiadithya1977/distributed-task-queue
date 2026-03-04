8a24a9d9e618d0f1cf2fb9219fd07dd00da87a0b
adithya <kuchanaadithya2006@gmail.com>
2026-03-04 16:20:58 +0000

    fix mail transporter config

--- services/mailService.js
+++ services/mailService.js
@@ -4,6 +4,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
+  secure:false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
@@ -15,7 +16,7 @@ export async function sendWelcomeEmail(user) {
   const info = await transporter.sendMail({
     from: process.env.MAIL_FROM,
     to: user.email,
-    subject: "Welcome to the Queue System 🚀",
+    subject: "Welcome to the Queue System ",
     html: `
       <h2>Hello ${user.username}</h2>
       <p>Your account was successfully registered.</p>
