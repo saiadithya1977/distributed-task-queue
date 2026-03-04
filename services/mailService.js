239b3c730dc17fa66fa0842f9c67ee084478daf4
adithya <kuchanaadithya2006@gmail.com>
2026-03-04 16:48:07 +0000

    fix mail service mock

--- services/mailService.js
+++ services/mailService.js
@@ -14,22 +14,11 @@ const transporter = nodemailer.createTransport({
 
 export async function sendWelcomeEmail(user) {
   console.log("Preparing email for:", user.email);
-  // const info = await transporter.sendMail({
-  //   from: process.env.MAIL_FROM,
-  //   to: user.email,
-  //   subject: "Welcome to the Queue System ",
-  //   html: `
-  //     <h2>Hello ${user.username}</h2>
-  //     <p>Your account was successfully registered.</p>
-  //     <p>This email was sent asynchronously by a background worker.</p>
-  //     <br/>
-  //     <b>Job Processing System Demo</b>
-  //   `,
-  // });
+
+  // simulate network delay
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
   console.log(`Email sent successfully to ${user.email}`);
 
-  console.log("Email delivered:", info.messageId);
   return true;
 }
